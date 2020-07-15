3a71a8551a1fab902ce2a9d90e59719e0cfabe95
pust.love008@gmail.com <pust.love008@gmail.com>
2020-07-15 18:23:04 +0600

    Aquila Started

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Main File and Blog
+ *
+ * @pakage Aquila
+ */
+;?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aquila</title>
+</head>
+<body>
+    <head>
+        <p>Header</p>
+    </head>
+    <div class="content">
+        <p>Content</p>
+    </div>
+
+    <footer>
+        <p>Footer</p>
+    </footer>
+</body>
+</html>
